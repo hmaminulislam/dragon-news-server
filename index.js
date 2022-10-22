dec2884914c6b303ba3247b22e62559f532fb7cf
Aminul <mdaminulislamsahin14@gmail.com>
2022-10-22 09:23:28 +0600

    news added

--- index.js
+++ index.js
@@ -5,13 +5,35 @@ const port = process.env.PORT || 5000
 
 app.use(cors());
 const categories = require('./data/categories.json')
+const news = require('./data/news.json')
 
 app.get('/', (req, res)=> {
-    res.send("Dragon News BN")
+    res.send("Dragon News Running")
 })
 
 app.get('/news-categories', (req, res) => {
     res.send(categories)
+})
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews)
+})
+
+app.get('/news', (req, res) => {
+    res.send(news)
+})
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if(id === '08') {
+        res.send(news)
+    }
+    else{
+        const selectedCategory = news.filter((n) => n.category_id === id);
+        res.send(selectedCategory);
+    }
 })
 
 app.listen(port, () => {
